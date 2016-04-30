f4808891826ac8c8d8a0867673174ed4ff23d969
edodo7 <massa-messo@hotmail.com>
2016-04-30 16:55:52 +0200

    initial commmit

--- src/GUI/testPanel.java
+++ src/GUI/testPanel.java
@@ -4,15 +4,10 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import board.Board;
