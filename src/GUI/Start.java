bdb0ad15259f260358c322174854ad34e222e568
edodo7 <massa-messo@hotmail.com>
2016-05-05 12:46:15 +0200

    Ecran d'acceuil

---
+++ src/GUI/Start.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Start extends JPanel {
+	
+	
+	public void paintComponent(Graphics g){
+		g.setColor(Color.BLUE);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Accueil",Font.BOLD,50));
+        g.drawString("Bienvenue dans le magnifique Quoridor d'Eduardo", this.getWidth()/9,this.getHeight()/3);
+	}
+}
